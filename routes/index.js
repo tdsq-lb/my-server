2e55743de092837fbf51c055e636a7296f824f0c
libiao <578556500@qq.com>
2020-11-11 17:59:28 +0800

    答题 和 排行榜接口

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const cheerio = require('cheerio');
 const db = require('../config/db');
 const loldetails = require('../util/lolconfig.json')
 
-/* GET home page. */
+/* 登陆用户信息 */
 router.get('/api/userinfo', function (req, res, next) {
   const promise = new Promise((resolve, reject) => {
     let gameid = encodeURI(req.query.gameid)
@@ -35,7 +35,10 @@ router.get('/api/userinfo', function (req, res, next) {
         userinfo.genting_Game_ranking = $('.main .main_box .main_cont .main_item:nth-child(3) .main_txt').text()
         userinfo.national_Service_Rank = $('.main .main_box .main_cont .main_item:nth-child(5) .main_txt').text()
         if (userinfo.avatar) {
-          db.query(`CALL p_user_Add('${userinfo.name}','${userinfo.area}')`, [], (res, fie) => {
+          let avatarurl = userinfo.avatar
+          const index = avatarurl.lastIndexOf("\/")
+          avatarurl = avatarurl.substring(index + 1, avatarurl.length)
+          db.query(`CALL p_user_Add('${userinfo.name}','${userinfo.area}','${avatarurl}')`, [], (res, fie) => {
             userinfo.id = res[0].id
             resolve(userinfo)
           })
@@ -54,10 +57,37 @@ router.get('/api/userinfo', function (req, res, next) {
 
 
 });
-
+/* 获取题目  */
 router.get('/api/topic', function (req, res, next) {
-  // Math.floor(Math.random() * items.length)
   const data = loldetails.list[Math.floor(Math.random() * loldetails.list.length)]
   res.send(data)
 })
+
+/* 回答题目 */
+router.get('/api/answer', function (req, res, next) {
+  const userid = req.query.userid
+  if (userid) {
+    const promise = new Promise((resolve, reject) => {
+      db.query(`CALL p_answer_Add('${userid}')`, [], (res, fie) => {
+        resolve(res)
+      })
+    })
+    promise.then((resolve) => {
+      res.send(resolve)
+    })
+  }
+})
+
+/* 获取排行榜 */
+router.get('/api/ranking', function (req, res, next) {
+  const promise = new Promise((resolve, reject) => {
+    db.query(`CALL p_ranking_list`, [], (res, fie) => {
+      resolve(res)
+    })
+  })
+  promise.then((resolve) => {
+    res.send(resolve)
+  })
+})
+
 module.exports = router;
